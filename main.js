83c35ef5b0f09ece4ac15a1aedc263576011008c
momose1031 <macbookair2020momose@icloud.com>
2021-08-24 13:29:07 +0900

    リトライボタン

--- main.js
+++ main.js
@@ -42,6 +42,9 @@ DHSelect.addEventListener('change', (e) => {
           alert('全角4文字以内で入力してください')
         }
         if (bytes(player.value) <= 8 && player.value) {
+          if (e.target.value !== 'select1') {
+            return;
+          }
           players.push(player.value);
           const li = document.createElement('li');
           li.textContent = player.value;
@@ -120,6 +123,97 @@ DHSelect.addEventListener('change', (e) => {
   }
   else if (e.target.value === 'select2') {
     DH.textContent = 'DHあり';
+    selectBtn.addEventListener('click', () => {
+      DHSelect.classList.add('hidden');
+      selectBtn.classList.add('hidden');
+      player.classList.remove('hidden');
+      btn.classList.remove('hidden');
+      btn.addEventListener('click', () => {
+        if (bytes(player.value) > 8) {
+          alert('全角4文字以内で入力してください')
+        }
+        if (bytes(player.value) <= 8 && player.value) {
+          if (e.target.value !== 'select2') {
+            return;
+          }
+          players.push(player.value);
+          const li = document.createElement('li');
+          li.textContent = player.value;
+          const deleteBtn = document.createElement('div');
+          deleteBtn.classList.add('delete-btn');
+          deleteBtn.textContent = '(削除する)';
+          playerLists.appendChild(li);
+          playerLists.appendChild(deleteBtn);
+          player.value = ''; //inputの中身を空にする
+          deleteBtn.addEventListener('click', () => {
+            li.remove();
+            deleteBtn.remove();
+            let index = players.indexOf(li.textContent);
+            if (index > -1) {
+              players.splice(index, 1);
+            }
+            if(players.length < 11) {
+              player.classList.remove('hidden');
+              btn.classList.remove('hidden');
+              changeBtn.classList.add('hidden');
+            }
+          });
+          if (players.length > 9) {
+            player.classList.add('hidden');
+            btn.classList.add('hidden');
+            changeBtn.classList.remove('hidden');
+          }
+        }
+      });
+      changeBtn.addEventListener('click', () => {
+        if (e.target.value !== 'select2') {
+          return;
+        }
+        const allDeleteBtn = document.getElementsByClassName('delete-btn');
+        [].forEach.call(allDeleteBtn, (deleteBtn) => {
+          deleteBtn.classList.add('hidden');
+        });
+        if (players.length === 0) {
+          return;
+        }
+        const player1 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        pitcher.textContent = player1;
+        const player2 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        chatcher.textContent = player2;
+        const player3 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        first.textContent = player3;
+        const player4 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        second.textContent = player4;
+        const player5 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        third.textContent = player5;
+        const player6 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        short.textContent = player6;
+        const player7 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        left.textContent = player7;
+        const player8 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        center.textContent = player8;
+        const player9 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        right.textContent = player9;
+        const player10 = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        DH.textContent = 'DH：' + player10;
+        changeBtn.classList.add('hidden');
+        retryBtn.classList.remove('hidden');
+        endBtn.classList.remove('hidden');
+        retryBtn.addEventListener('click', () => {
+          players.push(player1, player2, player3, player4, player5, player6, player7, player8, player9, player10);
+          pitcher.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          chatcher.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          first.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          second.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          third.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          short.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          left.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          center.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          right.textContent = players.splice(Math.floor(Math.random() * players.length), 1)[0];
+          DH.textContent = 'DH：' + players.splice(Math.floor(Math.random() * players.length), 1)[0];
+        });
+      });
+    });
   }
   else {
     DH.textContent = 'DHを選択';
